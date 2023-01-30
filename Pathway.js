d424f29dc332b9ca51288e745cc442c49c0541e4
Lefido <lefido@hotmail.com>
2023-01-30 15:43:25 +0100

    first commit

---
+++ Pathway.js
@@ -0,0 +1,53 @@
+
+export const pathway = [
+
+    {
+            
+
+        choice: [
+        
+            ["Commencer la partie ?", 1]
+
+        ]
+        
+        
+
+    },
+
+        {
+            
+
+            choice: [
+            
+                ["Prétendre être un herboriste ?", 261],
+                 
+                ["Vous faire passer pour un marchand ?", 230],
+                 
+                ["Demander l'hospitalité pour la nuit ?", 23]
+
+            ]
+            
+            
+
+        },
+
+        {
+
+            choice: [
+            
+                ["Allez-vous essayer d'ouvrir la porte ?", 142],
+                 
+                ["vous poursuivre votre chemin ?", 343],
+
+                ["Je vais l'attaquer !", 200]
+
+            ]
+          
+        }
+
+    
+]
+
+// {choix: "Prétendre être un herboriste ?" , id_dest: 261},
+// {choix: "Vous faire passer pour un marchand ? " , id_dest: 230},
+// {choix: "Demander l'hospitalité pour la nuit ?" , id_dest: 20}
